53f8c0377326b3b46876107d49cecf8f02a299e2
Nitesh Kumar <niteshkr1126@gmail.com>
2018-02-09 01:50:44 +0530

    Add files via upload

---
+++ src/main/java/com/nk/travel/FetchHotelsUrlByCity.java
@@ -0,0 +1,26 @@
+package com.nk.travel;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FetchHotelsUrlByCity {
+
+	public static void main(String[] args) {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader("./input/City_Hotels_URLs.txt"));
+			String line = reader.readLine();
+			while (line != null) {
+				String[] lineArray = line.split(" : ");
+				String city = lineArray[0];
+				String cityUrl = lineArray[1];
+				System.out.println(cityUrl);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
