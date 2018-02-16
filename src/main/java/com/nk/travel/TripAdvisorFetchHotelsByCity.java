c3650fb5128d1e25950d71aa42bd491143f680f1
Nitesh Kumar <niteshkr1126@gmail.com>
2018-02-16 19:04:41 +0530

    Update TripAdvisorFetchHotelsByCity.java

--- src/main/java/com/nk/travel/TripAdvisorFetchHotelsByCity.java
+++ src/main/java/com/nk/travel/TripAdvisorFetchHotelsByCity.java
@@ -1,7 +1,9 @@
 package org.jsoup;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
@@ -30,18 +32,13 @@ public class TripAdvisorFetchHotelsByCity {
 		String sCityUrl;
 		FileReader fr = new FileReader(input_city_urls);
 		BufferedReader br = new BufferedReader(fr);
-		//FileWriter fw = new FileWriter(output_hotel_urls);
-		//BufferedWriter bw = new BufferedWriter(fw);
+		
 		try {
 			while ((sCityUrl = br.readLine()) != null) {
 				break;
 			}
 		} finally {
 			try {
-				/*if (bw != null)
-					bw.close();
-				if (fw != null)
-					fw.close();*/
 				if (fr != null)
 					fr.close();
 				if (br != null)
@@ -72,36 +69,51 @@ public class TripAdvisorFetchHotelsByCity {
 	public static void main(String[] args) throws Exception {
 		TripAdvisorFetchHotelsByCity rf  = new TripAdvisorFetchHotelsByCity();
 		String cityurl = rf.readFile();
-		
-		SSLContext ctx = SSLContext.getInstance("TLS");
-		ctx.init(new KeyManager[0], new TrustManager[] { new DefaultTrustManager() }, new SecureRandom());
-		SSLContext.setDefault(ctx);
-		URL url = new URL(cityurl);
-		Proxy proxy = new Proxy(Proxy.Type.HTTP,
-				new InetSocketAddress("xxxxx", 80));
-		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection(proxy);
-		conn.connect();
-		String line = null;
-		StringBuffer tmp = new StringBuffer();
-		HtmlCleaner htmlCleaner = new HtmlCleaner();
-		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		while ((line = in.readLine()) != null) {
-			tmp.append(line);
-		}
-		TagNode[] root = htmlCleaner.clean(String.valueOf(tmp)).getElementsByName("div", true);
-		for (TagNode hotelLists : root) {
-			if (hotelLists.getAttributeByName("id") != null) {
-				if (hotelLists.getAttributeByName("id").equalsIgnoreCase("taplc_hsx_hotel_list_lite_dusty_hotels_combined_sponsored_0")) {
-					TagNode[] hotelUrls = hotelLists.getElementsByName("a", true);
-					for (TagNode hotelUrl : hotelUrls) {
-						if (hotelUrl.getAttributeByName("class") != null) {
-							if (hotelUrl.getAttributeByName("class").equalsIgnoreCase("property_title prominent")) {
-								String hotellink = "https://www.tripadvisor.in" + hotelUrl.getAttributeByName("href");
-								System.out.println(hotellink);
+		FileWriter fw = new FileWriter(output_hotel_urls);
+		BufferedWriter bw = new BufferedWriter(fw);
+		try {
+			SSLContext ctx = SSLContext.getInstance("TLS");
+			ctx.init(new KeyManager[0], new TrustManager[] { new DefaultTrustManager() }, new SecureRandom());
+			SSLContext.setDefault(ctx);
+			URL url = new URL(cityurl);
+			Proxy proxy = new Proxy(Proxy.Type.HTTP,
+					new InetSocketAddress("xxxx", 80));
+			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection(proxy);
+			conn.connect();
+			String line = null;
+			StringBuffer tmp = new StringBuffer();
+			HtmlCleaner htmlCleaner = new HtmlCleaner();
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			while ((line = in.readLine()) != null) {
+				tmp.append(line);
+			}
+			TagNode[] root = htmlCleaner.clean(String.valueOf(tmp)).getElementsByName("div", true);
+			for (TagNode hotelLists : root) {
+				if (hotelLists.getAttributeByName("id") != null) {
+					if (hotelLists.getAttributeByName("id").equalsIgnoreCase("taplc_hsx_hotel_list_lite_dusty_hotels_combined_sponsored_0")) {
+						TagNode[] hotelUrls = hotelLists.getElementsByName("a", true);
+						for (TagNode hotelUrl : hotelUrls) {
+							if (hotelUrl.getAttributeByName("class") != null) {
+								if (hotelUrl.getAttributeByName("class").equalsIgnoreCase("property_title prominent")) {
+									String hotellink = "https://www.tripadvisor.in" + hotelUrl.getAttributeByName("href");
+									System.out.println(hotellink);
+									bw.write(hotellink+"\n");
+								}
 							}
 						}
 					}
 				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bw != null)
+					bw.close();
+				if (fw != null)
+					fw.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
 			}
 		}
 	}
